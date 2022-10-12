3d010025eb2710ae1c6efe1de8d5463aaf8db493
Mark Todd <marktodd.hl@gmail.com>
2022-10-12 18:07:18 +0200

    Add Mdx example

--- src/content/sections/contact.md
+++ src/content/sections/contact.md
@@ -4,4 +4,10 @@ email: 'maxmustermann@gmail.com'
 address: 'Friesenstr. 123, Berlin, Germany'
 ---
 
+<script>
+  import Button from '$lib/components/Button.svelte';
+</script>
+
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy [Link to issues](https://github.com/Knochenmark/gatsby-starter-level-2/issues) tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+
+<Button text="Testing MDX" />
--- src/lib/utils/markdown.ts
+++ src/lib/utils/markdown.ts
@@ -4,19 +4,6 @@ export async function getSectionsContent(section) {
       `../../../src/content/sections/${section}.md`
     );
 
-    // TODO: Test below code esp. line 7-9
-    // export async function load({ params }){
-    //   const post = await import(`../${params.slug}.md`)
-    //   const { title, date } = post.metadata
-    //   const content = post.default
-
-    //   return {
-    //     content,
-    //     title,
-    //     date,
-    //   }
-    // }
-
     return { content, ...metadata };
   } catch (error) {
     console.log(`Error importing sections content for ${section}: ${error}`);
