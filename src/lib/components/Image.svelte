d217890737d3398d13b11763c04f904787b06c2d
Mark Todd <marktodd.hl@gmail.com>
2022-10-17 17:48:35 +0200

    Fix image hover styles

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -1,15 +1,20 @@
 <script>
-  export let src; 
-  export let alt; 
-  export let width=''; 
-  export let height=''; 
-  export let animated=false;
+  export let src;
+  export let alt;
+  export let width = '';
+  export let height = '';
+  export let animated = false;
+  export let decorated = false;
 </script>
 
-<img width={width} height={height} alt={alt} src={src} class:animated />
+<div class="image-wrapper" class:animated class:decorated>
+  <img {width} {height} {alt} {src} />
+</div>
 
 <style lang="sass">
   @import '../styles/mixins.sass'
   .animated
-    @include hover-scale
+    @include img-hover-scale
+  .decorated
+    @include img-border-decoration
 </style>
